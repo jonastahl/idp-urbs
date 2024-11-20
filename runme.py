57647e880fd82788978f095dc7e42a48e020b7ab
merhart <m.erhart@tum.de>
2024-11-20 14:56:42 +0100

    added additional results in response

--- runme.py
+++ runme.py
@@ -1,10 +1,17 @@
 import os
 import shutil
 from collections import defaultdict
+import json
+import sys
+sys.path.append("./urbs")
+from input import get_input
+from output import get_constants, get_timeseries
+from util import is_string
 
 import urbs
 from urbs import get_constants
 
+sys.path.append("/home/merhart/Documents/vurbs/lib/python3.12/site-packages/glpk.cpython-312-x86_64-linux-gnu.so")
 
 def run(config):
     result_name = 'Run'
@@ -57,11 +64,94 @@ def run(config):
     def default():
         return defaultdict(default)
     proc = default()
+    print("REPORTING")
     for ((year, site, commodity), row) in cpro.iterrows():
         proc[site][commodity]['New'] = row['New']
         proc[site][commodity]['Total'] = row['Total']
 
+    # TESTING ONLY
+    # quick and dirty 
+    #
+    
+    report_tuples = get_input(prob, 'demand').columns
+
+
+
+    print(report_tuples)
+
+    energies = []
+    timeseries = {}
+    help_ts = {}
+
+    # collect timeseries data
+    for stf, sit, com in report_tuples:
+
+        # wrap single site name in 1-element list for consistent behavior
+        if is_string(sit):
+            help_sit = [sit]
+        else:
+            help_sit = sit
+            sit = tuple(sit)
+
+        # check existence of predefined names, else define them
+        try:
+            report_sites_name[sit]
+        except BaseException:
+            report_sites_name[sit] = str(sit)
+
+        for lv in help_sit:
+            (created, consumed, stored, imported, exported,
+                dsm, voltage_angle) = get_timeseries(instance, stf, com, lv)
+
+            overprod = pd.DataFrame(
+                columns=['Overproduction'],
+                data=created.sum(axis=1) - consumed.sum(axis=1) +
+                imported.sum(axis=1) - exported.sum(axis=1) +
+                stored['Retrieved'] - stored['Stored'])
+
+            tableau = pd.concat(
+                [created, consumed, stored, imported, exported, overprod,
+                    dsm, voltage_angle],
+                axis=1,
+                keys=['Created', 'Consumed', 'Storage', 'Import from',
+                        'Export to', 'Balance', 'DSM', 'Voltage Angle'])
+            help_ts[(stf, lv, com)] = tableau.copy()
+
+            # timeseries sums
+            help_sums = pd.concat([created.sum(), consumed.sum(),
+                                    stored.sum().drop('Level'),
+                                    imported.sum(), exported.sum(),
+                                    overprod.sum(), dsm.sum()],
+                                    axis=0,
+                                    keys=['Created', 'Consumed', 'Storage',
+                                        'Import', 'Export', 'Balance',
+                                        'DSM'])
+            print(help_sums)
+            try:
+                timeseries[(stf, report_sites_name[sit], com)] = \
+                    timeseries[(stf, report_sites_name[sit], com)].add(
+                    help_ts[(stf, lv, com)], axis=1, fill_value=0)
+                sums = sums.add(help_sums, fill_value=0)
+            except BaseException:
+                timeseries[(stf, report_sites_name[sit], com)] = help_ts[
+                    (stf, lv, com)]
+                sums = help_sums
+
+        # timeseries sums
+        sums = pd.concat([created.sum(), consumed.sum(),
+                            stored.sum().drop('Level'),
+                            imported.sum(), exported.sum(), overprod.sum(),
+                            dsm.sum()],
+                            axis=0,
+                            keys=['Created', 'Consumed', 'Storage', 'Import',
+                                'Export', 'Balance', 'DSM'])
+        energies.append(sums.to_frame("{}.{}.{}".format(stf, sit, com)))
+
+        print(energies)
+
     return {
         'costs': costs.to_dict(),
-        'process': proc
+        'process': proc,
+        'sum': json.dumps(energies),
+        "test": "test"
     }

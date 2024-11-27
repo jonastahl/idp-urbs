39145d3d0ae6c597e9ea844b0cc2e07e099cd1b3
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2024-11-27 13:08:18 +0100

    ✨ Add stupid buffer

--- server.py
+++ server.py
@@ -3,10 +3,19 @@ from runme import run
 
 app = Flask(__name__)
 
+buffer = dict()
+
 @app.post('/simulate')
 def simulate():
+    global buffer
+    if request.data in buffer:
+        print("Found in buffer")
+        return buffer[request.data]
+    print("Not found in buffer -> simulating")
     config = request.get_json()
-    return run(config)
+    result = run(config)
+    buffer[request.data] = result
+    return result
 
 if __name__ == '__main__':
     app.run()

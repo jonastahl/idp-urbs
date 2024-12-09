83d2541b09b46bd140dd615f73219b2a75248b29
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2024-12-09 18:02:17 +0100

    ✨ Report result to callback id

--- server.py
+++ server.py
@@ -1,21 +1,19 @@
+import threading
+
+import requests
 from flask import Flask, request
 from runme import run
 
 app = Flask(__name__)
 
-buffer = dict()
-
 @app.post('/simulate')
-def simulate():
-    global buffer
-    if request.data in buffer:
-        print("Found in buffer")
-        return buffer[request.data]
-    print("Not found in buffer -> simulating")
-    config = request.get_json()
-    result = run(config)
-    buffer[request.data] = result
-    return result
+def trigger_simulation():
+    threading.Thread(target=simulate, args=[request.get_json()]).start()
+    return "Simulation started"
+
+
+def simulate(config):
+    requests.post(config['callback'], json=run(config))
 
 if __name__ == '__main__':
     app.run()
